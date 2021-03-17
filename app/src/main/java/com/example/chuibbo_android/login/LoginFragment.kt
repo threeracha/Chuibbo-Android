2ea6ccee21bcc6edbcd0ad4f698e53cef8b95f51
sss20young <ksyt98@gmail.com>
2021-03-17 14:37:07 +0900

    sy: create dialog & connect login and signup

--- app/src/main/java/com/example/chuibbo_android/login/LoginFragment.kt
+++ app/src/main/java/com/example/chuibbo_android/login/LoginFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.chuibbo_android.R
+import com.example.chuibbo_android.signup.SignupFragment
+import kotlinx.android.synthetic.main.login_fragment.*
 
 class LoginFragment : Fragment() {
 
@@ -13,7 +15,24 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.login_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var dialog: LoginFailureDialogFragment = LoginFailureDialogFragment()
+        dialog.isCancelable = false // dialog 영영 밖(외부) 클릭 시, dismiss되는 현상 막기
+
+        login_button.setOnClickListener {
+            activity?.supportFragmentManager?.let { it1 -> dialog.show(it1, "Login Failure") }
+        }
+
+        make_account_text.setOnClickListener{
+            activity?.supportFragmentManager?.beginTransaction()?.apply {
+                replace(R.id.frameLayout, SignupFragment())
+                addToBackStack(null)
+            }?.commit()
+        }
     }
 }
