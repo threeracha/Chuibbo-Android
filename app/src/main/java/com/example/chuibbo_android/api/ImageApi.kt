a79be7d0a03ecb13411d2f584805209b4b5ad647
jennkimm <seohkim@student.42seoul.kr>
2021-07-23 20:53:15 +0900

    feat: add option selection function

--- app/src/main/java/com/example/chuibbo_android/api/ImageApi.kt
+++ app/src/main/java/com/example/chuibbo_android/api/ImageApi.kt
@@ -1,13 +1,13 @@
 package com.example.chuibbo_android.api
 
+import com.example.chuibbo_android.api.request.ResumePhotoUploadRequest
 import com.example.chuibbo_android.api.response.ApiResponse
 import okhttp3.MultipartBody
+import okhttp3.Request
+import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Multipart
-import retrofit2.http.POST
-import retrofit2.http.Part
+import retrofit2.http.*
 
 interface ImageApi {
     // 테스트
@@ -18,7 +18,8 @@ interface ImageApi {
     @Multipart
     @POST("/api/resume_photo/")
     fun uploadResumePhoto(
-        @Part image: MultipartBody.Part
+        @Part(encoding = "multipart") photo: MultipartBody.Part,
+        @PartMap(encoding = "params") data: HashMap<String, RequestBody>
     ): Call<ResponseBody>
 
     companion object {
