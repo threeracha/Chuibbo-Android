0c8b00f97d80929a45b036e9352f0cf0f4a34872
sss20young <ksyt98@gmail.com>
2021-11-16 22:16:52 +0900

    Feat: connect mypage to bookmark api

--- app/src/main/java/com/example/chuibbo_android/api/JobPostApi.kt
+++ app/src/main/java/com/example/chuibbo_android/api/JobPostApi.kt
@@ -4,8 +4,7 @@ import android.content.Context
 import com.example.chuibbo_android.api.response.SpringResponse2
 import com.example.chuibbo_android.home.JobPost
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface JobPostApi {
 
@@ -19,6 +18,16 @@ interface JobPostApi {
 
     @GET("/api/v1/job_post/bookmarks")
     fun getBookmarks(): Call<SpringResponse2<List<JobPost>>>
+
+    @POST("/api/v1/job_post/{job_post_id}/bookmark")
+    fun saveBookmark(
+        @Path("job_post_id") id: Int
+    ): Call<SpringResponse2<String>>
+
+    @DELETE("/api/v1/job_post/{job_post_id}/bookmark")
+    fun deleteBookmark(
+        @Path("job_post_id") id: Int
+    ): Call<SpringResponse2<String>>
 
     companion object {
         val instance = ApiGenerator().generateSpring2(JobPostApi::class.java)
--- app/src/main/java/com/example/chuibbo_android/mypage/LikeJobPostAdapter.kt
+++ app/src/main/java/com/example/chuibbo_android/mypage/LikeJobPostAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.chuibbo_android.mypage
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +11,8 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chuibbo_android.R
+import com.example.chuibbo_android.api.JobPostApi
+import com.example.chuibbo_android.api.response.SpringResponse2
 import com.example.chuibbo_android.home.ImageLoader
 import com.example.chuibbo_android.home.JobPost
 import kotlinx.android.synthetic.main.mypage_like_job_posting.view.*
@@ -16,16 +20,19 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.time.LocalDate
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.time.LocalDateTime
-import java.time.Period
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 class LikeJobPostAdapter(private val onClick: (JobPost) -> Unit) :
     ListAdapter<JobPost, LikeJobPostAdapter.LikeJobPostViewHolder>(LikeJobPostDiffCallback) {
 
     /* ViewHolder for LikeJobPost, takes in the inflated view and the onClick behavior. */
-    class LikeJobPostViewHolder(itemView: View, val onClick: (JobPost) -> Unit) :
+    class LikeJobPostViewHolder(itemView: View, val onClick: (JobPost) -> Unit, context: Context) :
         RecyclerView.ViewHolder(itemView) {
         private val companyName: TextView = itemView.company_name
         private val companyDesc: TextView = itemView.company_desc
@@ -39,9 +46,42 @@ class LikeJobPostAdapter(private val onClick: (JobPost) -> Unit) :
                     onClick(it)
                 }
             }
+
             itemView.star.setOnClickListener {
-                currentLikeJobPost?.let {
-                    onClick(it)
+                if (itemView.star.drawable.constantState == context.resources.getDrawable(R.drawable.ic_star_fill).constantState) {
+                    JobPostApi.instance(context).deleteBookmark(currentLikeJobPost!!.id).enqueue(object :
+                        Callback<SpringResponse2<String>> {
+                        override fun onFailure(call: Call<SpringResponse2<String>>, t: Throwable) {
+                            Log.d("retrofit fail", t.message)
+                        }
+
+                        override fun onResponse(
+                            call: Call<SpringResponse2<String>>,
+                            response: Response<SpringResponse2<String>>
+                        ) {
+                            if (response.isSuccessful) {
+                                itemView.star.setImageResource(R.drawable.ic_star_empty)
+                                // TODO: datasource에서 remove
+                            }
+                        }
+                    })
+                } else {
+                    JobPostApi.instance(context).saveBookmark(currentLikeJobPost!!.id).enqueue(object :
+                        Callback<SpringResponse2<String>> {
+                        override fun onFailure(call: Call<SpringResponse2<String>>, t: Throwable) {
+                            Log.d("retrofit fail", t.message)
+                        }
+
+                        override fun onResponse(
+                            call: Call<SpringResponse2<String>>,
+                            response: Response<SpringResponse2<String>>
+                        ) {
+                            if (response.isSuccessful) {
+                                itemView.star.setImageResource(R.drawable.ic_star_fill)
+                                // TODO: datasource에서 add
+                            }
+                        }
+                    })
                 }
             }
         }
@@ -53,16 +93,7 @@ class LikeJobPostAdapter(private val onClick: (JobPost) -> Unit) :
             companyName.text = likeJobPost.companyName
             companyDesc.text = likeJobPost.subject
 
-            // TODO: day만 계산되고, month, year은 계산 안됨
-            val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-            val date = LocalDateTime.parse(likeJobPost?.endDate.replace("T", " "), formatter)
-            val period = Period.between(LocalDate.now(), date.toLocalDate())
-            if (period.getDays() >= 1)
-                companyDeadline.text = "D-" + period.getDays()
-            else if (period.getDays() == 0)
-                companyDeadline.text = "D-Day"
-            else
-                companyDeadline.text = "마감"
+            companyDeadline.text = calculateDday(likeJobPost.endDate)
 
             if (likeJobPost.logoUrl != null) {
 
@@ -74,12 +105,29 @@ class LikeJobPostAdapter(private val onClick: (JobPost) -> Unit) :
                 }
             }
         }
+
+        private fun calculateDday(end: String): String {
+            val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+            val endDateTime = LocalDateTime.parse(end.replace("T", " "), formatter)
+            val todayDateTime = LocalDateTime.now(ZoneId.of("Asia/Seoul"))
+            var difference = ChronoUnit.DAYS.between(todayDateTime.toLocalDate(), endDateTime.toLocalDate()).toInt() // 날짜만 계산
+
+            if (endDateTime.isAfter(todayDateTime)) {
+                if (endDateTime.year == todayDateTime.year && endDateTime.monthValue == todayDateTime.monthValue
+                    && endDateTime.dayOfMonth == todayDateTime.dayOfMonth) {
+                    return "D-Day"
+                } else {
+                    return "D-$difference"
+                }
+            } else
+                return "마감"
+        }
     }
 
     /* Creates and inflates view and return LikeJobPostViewHolder. */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LikeJobPostViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.mypage_like_job_posting, parent, false)
-        return LikeJobPostViewHolder(view, onClick)
+        return LikeJobPostViewHolder(view, onClick, parent.context)
     }
 
     /* Gets current likeJobPost and uses it to bind view. */
